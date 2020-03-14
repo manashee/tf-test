48e3c3cc3359743f59a45d257ebac82f647f1052
ashok <ashok.kumar@dbaux.com>
2020-03-14 14:27:47 +0530

    added user data

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 provider "aws" {
- region = "us-east-1"
- shared_credentials_file = "/Users/ashokkumar/.aws/credentials"
+  region = "us-east-1"
+  shared_credentials_file = "/Users/ashokkumar/.aws/credentials"
 }
 
 resource "aws_instance" "ec2-test" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
-  tags = { 
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "hello world" > index.html
+            nohup busybox - f - p 8080 &
+            EOF
+
+  tags = {
     Name = "ashok"
   }
 }
